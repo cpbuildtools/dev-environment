9a75050bf2dacd87ae3e0fd2425d905a059d7085
Daniel Erdelt <erd@conceptual.ca>
2022-05-16 17:37:40 +0000

    resume for win 10

--- installers/win_10/install.cmd
+++ installers/win_10/install.cmd
@@ -11,7 +11,7 @@ wsl --set-default-version 2
 
 install.cmd
 
-goto end
+goto :eof
 
 :enableWSL
 
@@ -21,15 +21,13 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 echo Enabling Virtual Machine Platform (this could also take a while)...
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce /v "!devenv-installer" /d "%temp%\devenv_intaller.cmd" /f
 echo.
 echo.
 echo.
 echo Enabled Windows Subsystem for Linux. Please restart your computer and run this script again.
 echo.
-echo.
+echo Press any key to restart, or press ctrl+c to exit.
 echo.
 pause
-
-goto end
-
-:end
+shutdown -r -t 0
