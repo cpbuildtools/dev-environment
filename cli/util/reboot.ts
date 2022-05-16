3fcee29266adb5c006b2a606438b63f4fc362c27
Daniel Erdelt <erd@conceptual.ca>
2022-05-16 19:28:38 +0000

    ...

--- cli/util/reboot.ts
+++ cli/util/reboot.ts
@@ -6,7 +6,7 @@ import { escapeString } from "./strings";
 
 
 export async function rebootWindows(resumeCommand?: string): Promise<never> {
-    await exec(`reg.exe add "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce" /v !devenvInstaller /d "${escapeString(resumeCommand)}" /f`);
+    await exec(`reg.exe add "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce" /v !devenvInstaller /d "${escapeString(resumeCommand)} && pause" /f`);
     console.info();
     console.info(chalk.yellow('********************************************************************'))
     console.info(chalk.yellow('* Windows need to be restarted... because windows...               *'))
