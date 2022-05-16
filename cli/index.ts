5c9597e13fb8b45c440d99d05c7999cea0702d31
Daniel Erdelt <erd@conceptual.ca>
2022-05-16 23:34:35 +0000

    ...

--- cli/index.ts
+++ cli/index.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env ts-node
 
+import { exit } from 'process';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 (async () => {
-    await yargs(hideBin(process.argv))
-        .scriptName('devenv')
-        .commandDir('./commands', { extensions: ['ts'], recurse: true })
-        .parse();
+    try{
+        await yargs(hideBin(process.argv))
+            .scriptName('devenv')
+            .commandDir('./commands', { extensions: ['ts'], recurse: true })
+            .parse();
+    }catch(e){
+        console.error(e);
+        exit(1);
+    }
 })();
