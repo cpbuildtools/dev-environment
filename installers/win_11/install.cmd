4ba2642268b85af8d63bda1416843006dfa5115f
Daniel Erdelt <erd@conceptual.ca>
2022-05-16 17:34:56 +0000

    resume after reboot

--- installers/win_11/install.cmd
+++ installers/win_11/install.cmd
@@ -4,23 +4,22 @@ wsl --update
 
 echo wsl --shutdown
 wsl --shutdown
-
 wsl --set-default-version 2
 
 install.cmd 
-goto end
+goto :eof
 
 :enableWSL
 
 wsl --install > nul 2>&1
 
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce /v "!devenv-installer" /d "%temp%\devenv_intaller.cmd" /f
 echo.
 echo.
 echo.
 echo Enabled Windows Subsystem for Linux. Please restart your computer and run this script again.
 echo.
-echo.
+echo Press any key to restart, or press ctrl+c to exit.
 echo.
 pause
-
-:end
+shutdown -r -t 0
