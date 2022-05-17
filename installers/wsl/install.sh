58f5f80c8a2b8ef8fa34e644044cf892b1cd7689
Daniel Erdelt <erd@conceptual.ca>
2022-05-17 21:52:27 +0000

    ...

--- installers/wsl/install.sh
+++ installers/wsl/install.sh
@@ -34,7 +34,7 @@ export PATH="$PNPM_HOME:$PATH"
 # Install Node JS Global Utils #
 ################################
 pnpm setup
-pnpm i -g typescript ts-node @type/node@16
+pnpm i -g typescript ts-node @types/node@16
 
 ################################
 # Clone Installer from Github  #
