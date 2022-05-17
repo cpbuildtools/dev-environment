9bfb2d9d2374063c519cdd64901a0bf7d6195310
Daniel Erdelt <erd@conceptual.ca>
2022-05-17 16:47:15 +0000

    ...

--- cli/util/vscode.ts
+++ cli/util/vscode.ts
@@ -1,9 +1,9 @@
 import { extname } from "path";
-import { exec } from "./cmd";
 
+import { spawn, exec } from 'child_process';
 
 export function launchVSCode(path: string = '.') {
-    return exec(`code ${path}`);
+    spawn(`code ${path}`, {detached: true, stdio: 'inherit'});
 }
 
 export function launchVSCodeDevContainer(containerPath: string = '.', open?: string) {
@@ -11,5 +11,25 @@ export function launchVSCodeDevContainer(containerPath: string = '.', open?: str
     const flag = isWS ? 'file-uri' : 'folder-uri';
     const hexPath = Buffer.from(containerPath).toString('hex');
     let uri = `vscode-remote://dev-container+${hexPath}/${open ?? ''}`;
-    return exec(`code --${flag} "${uri}"`);
+    //return exec(`code --${flag} "${uri}"`);
+
+    spawn(`code --${flag} "${uri}"`, {detached: true, stdio: 'inherit'});
 }
+
+/*
+
+export function exec(cmd: string, { cwd }: { cwd?: string } = {}): Promise<number> {
+    return new Promise((res, rej) => {
+        try {
+            const child = spawn(cmd, { shell: true, stdio: 'inherit', cwd, env: process.env });
+            child.on('exit', (code) => {
+                res(code ?? 0);
+            });
+        }
+        catch (e) {
+            rej(e);
+        }
+    });
+}
+
+*/
