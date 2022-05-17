1df2ca5126ad966db57a52fb43a396b8491791a1
Daniel Erdelt <erd@conceptual.ca>
2022-05-17 21:33:40 +0000

    ...

--- installers/win/install.cmd
+++ installers/win/install.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Detecting Ubuntu
 winget list Canonical.Ubuntu.2004 > nul 2>&1 && goto ubuntuInstalled
-
+echo Install
 echo [96mInstalling Ubuntu 20.04[0m
 winget install Canonical.Ubuntu.2004 --accept-package-agreements --accept-source-agreements
 rem Get the file name of the ubuntu executable
@@ -25,6 +25,7 @@ taskkill /IM "ubuntu2004.exe" /F > nul 2>&1
 wsl -t Ubuntu-20.04
 
 :ubuntuInstalled
+echo ubuntuInstalled
 echo [96mDownloading Ubuntu [93m20.04[96m Installer...[0m
 
 wsl -d Ubuntu-20.04 --cd ~ bash -ic "rm -f ~/dev-env-installer.sh"
@@ -33,5 +34,3 @@ wsl -d Ubuntu-20.04 --cd ~ bash -ic "chmod +x ~/dev-env-installer.sh"
 wsl -d Ubuntu-20.04 --cd ~ bash -ic "./dev-env-installer.sh"
 wsl -d Ubuntu-20.04 --cd ~ bash -ic "cd ~/devenv-cli && ./install.sh"
 wsl -d Ubuntu-20.04 --cd ~ bash -ic "devenv install --appdata=\"%appdata%\"
-
-"
